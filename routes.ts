d99841febf20a5010eb6d7a9e20913d5721ef4c6
Luiz Fernando <liko.12@hotmail.com>
2021-04-25 12:58:20 -0300

    aula 3 finish

--- routes.ts
+++ routes.ts
@@ -1,4 +1,5 @@
 import { Router} from "express"
+import { MessagesController } from "./src/controllers/MessagesController";
 import { SettingsController } from "./src/controllers/SettingsController";
 import { UsersController } from "./src/controllers/UserController";
 
@@ -7,8 +8,14 @@ const routes = Router();
 
 const settingsController = new SettingsController()
 const usersController = new UsersController()
+const messagesController = new MessagesController()
 
 routes.post('/settings',settingsController.create);
+
 routes.post('/users',usersController.create);
+
+routes.post('/message',messagesController.create);
+
+routes.get('/message/:id',messagesController.showByUser);
 
 export {routes};
--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,12 +1,17 @@
-import { getCustomRepository } from "typeorm"
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User";
 import { UsersRepository } from "../repositories/UsersRepository"
 
 
 class UsersService{
+  private usersRepository:Repository<User>
+  constructor(){
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
   async create(email:string){
-    const usersRepository = getCustomRepository(UsersRepository);
+    
     //Verifica se user existe
-    const  userExists = await usersRepository.findOne({
+    const  userExists = await this.usersRepository.findOne({
       email
 
     })
@@ -16,10 +21,10 @@ class UsersService{
 
     }
     // Se não existir salva no DB
-    const user = usersRepository.create({
+    const user = this.usersRepository.create({
       email,
     });
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
 
     return user
 
